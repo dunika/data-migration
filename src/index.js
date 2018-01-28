25aee21300429e161a82611a5c1ed456949d100f
dunika <dbreen91@gmail.com>
2018-01-28 21:39:03 +0000

    BEgan rwefactoring queries

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const { values } = require('lodash')
 
 const mysql = require('promise-mysql');
 
+const taxonomyQueries = require('./queries/taxonomies')
 const queries = require('./queries')
 const getKeys = require('./get-keys')
 const {
@@ -17,50 +18,92 @@ const writeJsonToFile = (name, json) => {
   writeFileSync(resolve(__dirname, `../data/${name}.json`), JSON.stringify(json, null, 2), 'utf8')
 }
 
+const addId = (data) => data.map((item, index) => ({ ...item, id: index }))
+const getIdByName = (name) => regions.find((name) => item.name === name).id
+
+const getRegions = async (connection) => {
+  const regions = await connection.query(taxonomyQueries.getRegions)
+  const regionsWithId = addId(regions)
+
+  const parentRegions = [{
+    name: 'Dublin',
+    id: getRegionId('Dublin'),
+  }, {
+    name: 'London',
+    id: getRegionId('London')
+  }]
+  
+  return regionsWithId.map(({ name, ...rest }) => {
+    const parentId = parentRegions.reduce((value, parent) => {
+      if (name.includes(parent.name)) {
+        return parent.id
+      }
+      return value
+    }, null)
+
+    return { name, parent: parentId, ...rest }
+  })
+}
+
+const getCategories = async (connection) => {
+  const categories = await connection.query(taxonomyQueries.getCategories)
+  return addId(categories)
+}
+
+const getTypes = async (connection) => {
+  const types = await connection.query(taxonomyQueries.getTypes)
+  return addId(types)
+}
+
+
 const main = async () => {
   try {
-    const  connection = await mysql.createConnection({
+    const connection = await mysql.createConnection({
       host: 'localhost',
       user: 'root',
       password: 'chachacha100',
       database: 'beseen_jalert'
     });
 
-    console.log('Getting data');
-    const userData = await connection.query(queries.getUserData)
-    const jobData = await connection.query(queries.getJobData)
-    const jobCategoryData = await connection.query(queries.getJobCategoryData)
-    const jobTypeData = await connection.query(queries.getJobTypeData)
-    const jobRegionData = await connection.query(queries.getJobRegionData)    
-    const applicationData = await connection.query(queries.getApplicationData)
+    const regions = await getRegions(connection)
+    const categories = await getCategories(connection)
+    const types = await getTypes(connection)
     
-    console.log('Transforming data');   
-    const transformedApplicationData = transformApplicationData(applicationData)
-    const transformedUserData = transformUserData(userData, transformedApplicationData) 
-    const transformedJobTypeData = transformTaxonomyData(jobTypeData)
-    const transformedJobCategoryData = transformTaxonomyData(jobCategoryData)
-    const transformedJobRegionData = transformTaxonomyData(jobRegionData)        
-    const transformedJobData = transformJobsData(
-      jobData,
-      transformedApplicationData,
-      transformedJobTypeData,
-      transformedJobCategoryData,
-      transformedJobRegionData
-    )
-
-    const jobs = values(transformedJobData)
-    const users = values(transformedUserData)
+    // console.log('Getting data');
+    // const userData = await connection.query(queries.getUserData)
+    // const jobData = await connection.query(queries.getJobData)
+    // const jobCategoryData = await connection.query(queries.getJobCategoryData)
+    // const jobTypeData = await connection.query(queries.getJobTypeData)
+    // const jobRegionData = await connection.query(queries.getJobRegionData)    
+    // const applicationData = await connection.query(queries.getApplicationData)
     
-    console.log('Getting keys')
-    const jobKeys = getKeys(jobs)
-    const userKeys = getKeys(users)
+    // console.log('Transforming data');   
+    // const transformedApplicationData = transformApplicationData(applicationData)
+    // const transformedUserData = transformUserData(userData, transformedApplicationData) 
+    // const transformedJobTypeData = transformTaxonomyData(jobTypeData)
+    // const transformedJobCategoryData = transformTaxonomyData(jobCategoryData)
+    // const transformedJobRegionData = transformTaxonomyData(jobRegionData)        
+    // const transformedJobData = transformJobsData(
+    //   jobData,
+    //   transformedApplicationData,
+    //   transformedJobTypeData,
+    //   transformedJobCategoryData,
+    //   transformedJobRegionData
+    // )
+    
+    // const jobs = values(transformedJobData)
+    // const users = values(transformedUserData)
+    
+    // console.log('Getting keys')
+    // const jobKeys = getKeys(jobs)
+    // const userKeys = getKeys(users)
 
-    console.log('Writing data');        
-    writeJsonToFile('users', users)
-    writeJsonToFile('user-keys', userKeys)
-    writeJsonToFile('jobs', jobs)
-    writeJsonToFile('job-keys', jobKeys)
-        
+    // console.log('Writing data');        
+    // writeJsonToFile('users', users)
+    // writeJsonToFile('user-keys', userKeys)
+    // writeJsonToFile('jobs', jobs)
+    // writeJsonToFile('job-keys', jobKeys)
+    
     console.log('Finished');
     return
   } catch (error) {
@@ -68,4 +111,4 @@ const main = async () => {
   }
 }
 
-main()
+//main()
---
+++ src/queries/taxonomies.js
@@ -0,0 +1,50 @@
+const buildQuery = (query) => query.replace(/\n/g, '')
+
+const buildJobToTaxonomyQuery = (key) => buildQuery(`
+  SELECT
+    terms.name, relation.object_id AS job_id
+  FROM 
+    beseen_jalert.wp_9thne3_term_taxonomy tax
+  LEFT JOIN
+    beseen_jalert.wp_9thne3_terms terms 
+  ON 
+    tax.term_id = terms.term_id
+  LEFT JOIN
+    beseen_jalert.wp_9thne3_term_relationships relation
+  ON tax.term_taxonomy_id = relation.term_taxonomy_id
+  WHERE tax.taxonomy = '${key}';
+`)
+
+const buildTaxonomyQuery = (key) => buildQuery(`
+  SELECT
+    DISTINCT terms.name
+  FROM 
+    beseen_jalert.wp_9thne3_term_taxonomy tax
+  LEFT JOIN
+    beseen_jalert.wp_9thne3_terms terms 
+  ON 
+    tax.term_id = terms.term_id
+  LEFT JOIN
+    beseen_jalert.wp_9thne3_term_relationships relation
+  ON tax.term_taxonomy_id = relation.term_taxonomy_id
+  WHERE tax.taxonomy = '${key}'
+  ORDER BY terms.name;
+`)
+
+const getRegions = buildTaxonomyQuery('job_listing_region')
+const getTypes = buildTaxonomyQuery('job_listing_type')
+const getCategories = buildTaxonomyQuery('job_listing_category');
+
+const getJobsToRegions = buildTaxonomyQuery('job_listing_region')
+const getJobsToCategories = buildTaxonomyQuery('job_listing_category');
+const getJobsToTypes = buildTaxonomyQuery('job_listing_type')
+
+module.exports = {
+  getRegions,
+  getTypes,
+  getCategories,
+  getJobsToCategories,
+  getJobsToRegions,
+  getJobsToTypes,
+}
+
